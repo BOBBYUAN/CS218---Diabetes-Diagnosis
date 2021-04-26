8e66b6d704d080b387ccaed1c6fc1c26314af59a
BOBBYUAN <bobyuanwangcheng@gmail.com>
2021-04-25 23:37:36 -0700

    first commit

---
+++ src/main/java/sjsu/edu/cs218project2/Cs218project2Application.java
@@ -0,0 +1,21 @@
+package sjsu.edu.cs218project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Cs218project2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs218project2Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello From CS218 Team 3";
+	}
+
+}
---
+++ src/test/java/sjsu/edu/cs218project2/Cs218project2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sjsu.edu.cs218project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs218project2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
